d419f3a4df4a179601924a3a3b28fb1f74ebb24d
Vijay Ram kp <135154698+Vijaybbbb@users.noreply.github.com>
2024-02-16 20:47:10 +0530

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
+
+/**
+ * ?  =====Import Components=====
+ */
+import Home from './Pages/Home';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <div>dfghhh</div>
-    </>
-  )
+    <div>
+      <Home />
+    </div>
+  );
 }
 
-export default App
+export default App;
